5f34de257edd2de1b3cc5a13556ad337ba48580b
Иванов Дмитрий Александрович (70177039) <vtb70177039@region.vtb.ru>
2024-07-06 10:39:49 +0300

    fdasfasd

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
+
+load_dotenv()
+
+ALLOWED_HOSTS = str(os.getenv('allowedhost'))
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('mem.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=ALLOWED_HOSTS, port=80)
